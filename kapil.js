b205c39aa1ef775af5bff48547dd74084d2c0979
Satyam799 <satyamtripathisvp@gmail.com>
2024-11-04 23:24:33 +0530

    Created a local commit

--- kapil.js
+++ kapil.js
@@ -1,7 +1,5 @@
 console.log('hi')
 console.log('changes made locally')
-<<<<<<< HEAD
-=======
 console.log('created a merge conflict')
 console.log('kapil from remote')
 >>>>>>> 7032c45e5894a87ca30876c20b9bf1a3d143c925
